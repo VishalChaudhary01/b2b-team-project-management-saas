fe783e77c06d1ae2f9ec1408b935b8df08a2367f
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-04-15 17:01:24 +0530

    initial frontend setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button';
+
+export default function App() {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold'>Hi there</h2>
+      <Button className='bg-[#504B38]'>Click me</Button>
+    </div>
+  );
+}
