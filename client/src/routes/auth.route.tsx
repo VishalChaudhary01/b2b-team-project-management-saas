2ab6746a68a34ca180524de5da131c224cb5f28a
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-04-18 18:12:30 +0530

    protect client routes

--- client/src/routes/auth.route.tsx
+++ client/src/routes/auth.route.tsx
@@ -1,5 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import useAuth from '@/hooks/api/use-auth';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { isAuthRoute } from './common/routePath';
+import { DashboardSkeleton } from '@/components/skeletons/dashboard-skeleton';
 
 export const AuthRoute = () => {
-  return <Outlet />;
+  const location = useLocation();
+  const { data: authData, isLoading } = useAuth();
+  const user = authData?.user;
+  const _isAuthRoute = isAuthRoute(location.pathname);
+  console.log('Auth route::', authData);
+
+  if (isLoading && !_isAuthRoute) return <DashboardSkeleton />;
+
+  if (!user) return <Outlet />;
+
+  return <Navigate to={`workspace/${user.currentWorkspace?._id}`} replace />;
 };
--- client/src/routes/protected.route.tsx
+++ client/src/routes/protected.route.tsx
@@ -1,5 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { DashboardSkeleton } from '@/components/skeletons/dashboard-skeleton';
+import useAuth from '@/hooks/api/use-auth';
+import { Navigate, Outlet } from 'react-router-dom';
 
 export const ProtectedRoute = () => {
-  return <Outlet />;
+  const { data: authData, isLoading } = useAuth();
+  console.log('Protected Route:: ', authData);
+  const user = authData?.user;
+
+  if (isLoading) return <DashboardSkeleton />;
+
+  return user ? <Outlet /> : <Navigate to='/' replace />;
 };
